8bde3034ed8a6bfe5d05187ce806ea7a4743f047
patrisampe <patriciasampedrogarcia@gmail.com>
2016-09-07 15:18:33 -0700

    ff

--- build/web/js/app.js
+++ build/web/js/app.js
@@ -789,11 +789,11 @@ As the Object it may have a boolean property allowToLogInConsole, which determin
 
 						// bits
 					    if(tx_bps >  999999999){
-							globalStats["tx_bps_value"] = tx_bps/1000000000;
+							globalStats["tx_bps_value"] = tx_bps
 							globalStats["tx_bps_units"] = "gbit/s";
 							log.console('try 5');
 						}
-						if(tx_bps < 1000){
+						else if(tx_bps < 1000){
 							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "bit/s";
 
@@ -801,16 +801,16 @@ As the Object it may have a boolean property allowToLogInConsole, which determin
 						}
 						// kbits
 						else if(tx_bps >= 1000 && tx_bps < 1000000){
-							globalStats["tx_bps_value"] = tx_bps/1000;
+							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "kbit/s";
 							log.console('try 3');
 						}
 						else if(tx_bps >= 1000000 && tx_bps < 1000000000){
-							globalStats["tx_bps_value"] = tx_bps/1000000;
+							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "mbit/s";
 						}
 						else if(tx_bps >  999999999){
-							globalStats["tx_bps_value"] = tx_bps/1000000000;
+							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "gbituu/s";
 							log.console('try 5');
 						}
--- src/gui/webapp/app/controllers/home.controller.js
+++ src/gui/webapp/app/controllers/home.controller.js
@@ -100,11 +100,11 @@
 
 						// bits
 					    if(tx_bps >  999999999){
-							globalStats["tx_bps_value"] = tx_bps/1000000000;
+							globalStats["tx_bps_value"] = tx_bps
 							globalStats["tx_bps_units"] = "gbit/s";
 							log.console('try 5');
 						}
-						if(tx_bps < 1000){
+						else if(tx_bps < 1000){
 							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "bit/s";
 
@@ -112,16 +112,16 @@
 						}
 						// kbits
 						else if(tx_bps >= 1000 && tx_bps < 1000000){
-							globalStats["tx_bps_value"] = tx_bps/1000;
+							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "kbit/s";
 							log.console('try 3');
 						}
 						else if(tx_bps >= 1000000 && tx_bps < 1000000000){
-							globalStats["tx_bps_value"] = tx_bps/1000000;
+							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "mbit/s";
 						}
 						else if(tx_bps >  999999999){
-							globalStats["tx_bps_value"] = tx_bps/1000000000;
+							globalStats["tx_bps_value"] = tx_bps;
 							globalStats["tx_bps_units"] = "gbituu/s";
 							log.console('try 5');
 						}
